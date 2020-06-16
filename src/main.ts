548788dd11641893f34e36ec40cd54603f512782
David Barreto <barretollano@gmail.com>
2020-06-16 13:33:28 -0400

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { multiply } from './utils1';
+
+export function uselessCalculation(x: number): number {
+  return multiply(x, 10);
+}
---
+++ src/utils2.ts
@@ -0,0 +1,7 @@
+export function multiply(base: number, multiplier: number): number {
+  return base * multiplier * 100;
+}
+
+export function stupidThing() {
+  return 0;
+}
